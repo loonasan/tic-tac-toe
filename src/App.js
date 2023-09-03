cb06303cbf6b145ba858a7410f6abc46a1112792
Lunar <loonas@kkumail.com>
2023-09-03 18:27:30 +0700

    button logic and recolor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
+import React, {useState} from "react";
+
 import "./App.css";
 
 import { Board } from "./components/board";
 
 function App() {
-  const board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"];
+
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [xPlaying, setXPlaying] = useState(true)
+
+  const handleBoxClick = (boxIdx) => {
+    const updatedBoard = board.map((value, idx) => {
+      if(idx === boxIdx) {
+        return xPlaying === true ? "X" : "O";
+      } else {
+        return value;
+      }
+    })
+
+    setBoard(updatedBoard);
+
+    setXPlaying(!xPlaying);
+  }
+
   return (
     <div>
-      <Board board={board} onClick={null}/>
+      <Board board={board} onClick={handleBoxClick}/>
     </div>
   );
 }
